05b9faa153638d47876cd90f9201feb002bc533f
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-03-08 14:38:27 +0100

    Af

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,7 @@
 
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ('Nadir', 'Olibol', 'Amsterdam');
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ('Baka', 'Bana', 'Amstelveen');
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ('Bart', 'Simpson', 'Amsterdam');
